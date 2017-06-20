b7ad4597ff7612f93d00b97c11dc7c1a85f61b7e
wrathematics <wrathematics@gmail.com>
2017-06-20 19:27:56 -0400

    add expand()

---
+++ R/expand.r
@@ -0,0 +1,27 @@
+#' @export
+expand = function(x)
+{
+  if (comm.rank() == 0)
+  {
+    dim = dim(x)
+    bcast(dim)
+    
+    size = comm.size()
+    id = get.jid(NROW(x), all=TRUE)
+    
+    for (i in 1:(size - 1L))
+    {
+      x.local = x[id[[i+1]], ]
+      send(x.local, rank.dest=i)
+    }
+    
+    x.local = x[id[[1]], ]
+  }
+  else
+  {
+    dim = bcast()
+    x.local = recv(rank.source=0)
+  }
+  
+  new("shaq", Data=x.local, nrows=dim[1L], ncols=dim[2L])
+}
