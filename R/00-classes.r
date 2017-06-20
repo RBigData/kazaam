0a5d3567828dadb72b4f7f401167071552b6e99b
wrathematics <wrathematics@gmail.com>
2017-06-20 19:27:45 -0400

    add shaq class

---
+++ R/00-classes.r
@@ -0,0 +1,26 @@
+#' Class shaq
+#' 
+#' Distributed tall matrix class.
+#' 
+#' @slot DATA
+#' The local submatrix.
+#' @slot nrows, ncols
+#' The global matrix dimension.
+#' 
+#' @name shaq-class
+#' @keywords Classes
+#' @docType class
+setClass(
+  Class="shaq", 
+  representation=representation(
+    Data="matrix",
+    nrows="numeric",
+    ncols="numeric"
+  ),
+  
+  prototype=prototype(
+    Data=matrix(nrow=0, ncol=0),
+    nrows=0,
+    ncols=0
+  )
+)
---
+++ R/pbdSHAQ-package.r
@@ -0,0 +1,15 @@
+#' Tall Matrices
+#' 
+#' TODO
+#' 
+#' This package requires an MPI library (OpenMPI, MPICH2, or LAM/MPI).
+#' 
+#' @importFrom pbdMPI allreduce bcast send recv comm.size comm.rank
+#' @import methods
+#' 
+#' @name pbdSHAQ-package
+#' @docType package
+#' @author Drew Schmidt \email{wrathematics@gmail.com}, Wei-Chen Chen, Mike Matheson, and George Ostrouchov.
+#' @references Programming with Big Data in R Website: \url{http://r-pbd.org/}
+#' @keywords Package
+NULL
