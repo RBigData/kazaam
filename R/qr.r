b03a0c4aa1ff4b8320445db2c0afbfa6f4be49cc
wrathematics <wrathematics@gmail.com>
2017-06-20 19:28:20 -0400

    add qr funcitons

---
+++ R/qr.r
@@ -0,0 +1,20 @@
+#' @export
+qr_R = function(x)
+{
+  cp = cp.shaq(x)
+  R = chol(cp)
+  
+  R
+}
+
+
+
+#' @export
+qr_Q = function(x)
+{
+  R = qr_R(x)
+  Q.local = x@Data %*% solve(R)
+  
+  Q = new("shaq", Data=Q.local, nrows=x@nrows, ncols=x@ncols)
+  Q
+}
