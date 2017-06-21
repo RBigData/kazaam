5c7e1c3a115bf2046ba6639ac4700431fde36e2b
wrathematics <wrathematics@gmail.com>
2017-06-21 10:48:42 -0400

    add constructor

---
+++ R/01-constructor.r
@@ -0,0 +1,21 @@
+#' @export
+shaq = function(Data, nrows, ncols, checks=TRUE)
+{
+  if (!is.matrix(Data))
+    comm.stop("argument 'Data' must be a matrix")
+  check.is.natnum(nrows)
+  check.is.natnum(ncols)
+  check.is.flag(check)
+  
+  if (checks)
+  {
+    rowcheck = allreduce(NROW(Data))
+    nrows = rowcheck
+    
+    colcheck = comm.all(NCOL(Data) == ncols)
+    if (!isTRUE(colcheck))
+      comm.stop("local column dimensions disagree across ranks")
+  }
+  
+  new("shaq", Data=Data, nrows=nrows, ncols=ncols)
+}
