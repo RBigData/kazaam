01d9f252e654d56389db9109727093ce14fe6db2
wrathematics <wrathematics@gmail.com>
2017-06-21 14:30:48 -0400

    add print method

---
+++ R/print.r
@@ -0,0 +1,29 @@
+print_shaq = function(x, ...)
+{
+  if (comm.rank() == 0)
+  {
+    cat(paste0("# A shaq: ", x@nrows, "x", x@ncols, " on ", comm.size(), " MPI ranks\n"))
+    print(head(Data(x)))
+    cat("# ...\n")
+  }
+}
+
+
+
+#' print
+#' 
+#' Print method for a shaq.
+#' 
+#' @param x,object
+#' A shaq.
+#' @param ...
+#' Ignored
+#' 
+#' @name print
+#' @rdname print
+#' @export
+setMethod("print", signature(x="shaq"), print_shaq)
+
+#' @rdname print
+#' @export
+setMethod("show", signature(object="shaq"), function(object) print_shaq(object))
