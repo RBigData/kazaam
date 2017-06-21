92005c50b6ba628afda6d52699308c7db08f253e
wrathematics <wrathematics@gmail.com>
2017-06-21 14:30:31 -0400

    add setters

---
+++ R/03-setters.r
@@ -0,0 +1,50 @@
+#' setters
+#' 
+#' Setter functions for shaq objects.
+#' 
+#' @details
+#' \code{Data<-} will perform checks on the inserted data and ensure that the
+#' number of columns match across processors (requiring communication).  It will
+#' also udpate the number of rows as necessary.
+#' 
+#' \code{DATA<-} will perform no checks, so use with caution.
+#' 
+#' @param x
+#' A shaq.
+#' @param Data
+#' The new data
+#' 
+#' @name setters
+#' @rdname setters
+NULL
+
+
+
+Data_set.shaq = function(x, Data, checks=TRUE)
+{
+  if (checks)
+  {
+    nrows = check.shaq(Data(x), nrows(x), ncols(x))
+    x@nrows = nrows
+  }
+  
+  x@Data = Data
+}
+
+DATA_set.shaq = function(x, Data) Data_set.shaq(x, Data, checks=FALSE)
+
+#' @rdname setters
+#' @export
+setGeneric(name="Data<-", useAsDefault=Data_set.shaq, package="pbdSHAQ")
+
+#' @rdname setters
+#' @export
+setMethod("Data<-", signature(x="shaq"), Data_set.shaq)
+
+#' @rdname setters
+#' @export
+setGeneric(name="DATA<-", useAsDefault=DATA_set.shaq, package="pbdSHAQ")
+
+#' @rdname setters
+#' @export
+setMethod("DATA<-", signature(x="shaq"), DATA_set.shaq)
