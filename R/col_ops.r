83331f7da4e89924fc9c712bfc5f2ad79f6129b8
wrathematics <wrathematics@gmail.com>
2017-06-21 15:28:15 -0400

    add col_ops docs

--- R/col_ops.r
+++ R/col_ops.r
@@ -14,6 +14,25 @@ colmean.shaq = function(x, na.rm = FALSE, dims = 1L)
 
 
 
+#' Column Operations
+#' 
+#' Column operations (currently sums/means) for shaq objects.
+#' 
+#' @param x
+#' A shaq.
+#' @param na.rm
+#' Should \code{NA}'s be removed?
+#' @param dims
+#' Ignored.
+#' 
+#' @return
+#' A regular vector.
+#' 
+#' @name col_ops
+#' @rdname col_ops
+NULL
+
+
 #' @export
 setMethod("colSums", signature(x="shaq"), colsum.shaq)
 
