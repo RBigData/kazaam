e394b972f66b5ae02eb036b32aa69fb7b6a3c83c
wrathematics <wrathematics@gmail.com>
2017-06-21 15:20:12 -0400

    add constructor docs

--- R/01-constructor.r
+++ R/01-constructor.r
@@ -1,17 +1,15 @@
-check.shaq = function(Data, nrows, ncols)
-{
-  colcheck = comm.all(NCOL(Data) == ncols)
-  if (!isTRUE(colcheck))
-    comm.stop("local column dimensions disagree across ranks")
-  
-  rowcheck = allreduce(NROW(Data))
-  nrows = rowcheck
-  
-  return(nrows)
-}
-
-
-
+#' shaq
+#' 
+#' Constructor for shaq objects.
+#' 
+#' @param Data
+#' The local submatrix.
+#' @param nrows, ncols
+#' The GLOBAL number of rows and columns.
+#' @param checks
+#' Logical. Should some basic dimension checks be performed?  Note that these
+#' require communication, and with many MPI ranks, could be expensive.
+#' 
 #' @export
 shaq = function(Data, nrows, ncols, checks=TRUE)
 {
@@ -25,4 +23,18 @@ shaq = function(Data, nrows, ncols, checks=TRUE)
     nrows = check.shaq(Data, nrows, ncols)
   
   new("shaq", Data=Data, nrows=nrows, ncols=ncols)
+}
+
+
+
+check.shaq = function(Data, nrows, ncols)
+{
+  colcheck = comm.all(NCOL(Data) == ncols)
+  if (!isTRUE(colcheck))
+    comm.stop("local column dimensions disagree across ranks")
+  
+  rowcheck = allreduce(NROW(Data))
+  nrows = rowcheck
+  
+  return(nrows)
 }
