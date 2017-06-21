77b1ae928292b00fe07931bf3f53095773e13ddb
wrathematics <wrathematics@gmail.com>
2017-06-21 12:12:15 -0400

    fix bizarre crossprod methods bug

--- R/crossprod.r
+++ R/crossprod.r
@@ -1,10 +1,16 @@
 cp.shaq = function(x, y = NULL)
 {
+  if (!is.null(y))
+    comm.stop("only implemented for y=NULL")
+  
   cp.local = crossprod(x@Data)
   allreduce(cp.local)
 }
 
 
 
+# #' @export
+# setGeneric(name="crossprod", useAsDefault=base::crossprod, package="pbdSHAQ")
+
 #' @export
-setMethod("crossprod", signature(x="shaq", y="NULL"), cp.shaq)
+setMethod("crossprod", signature(x="shaq"), cp.shaq)
