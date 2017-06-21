c8b15225281613aeb4399a2b70c74c457acf0710
wrathematics <wrathematics@gmail.com>
2017-06-21 14:50:23 -0400

    fixed bracketing

--- R/bracket.r
+++ R/bracket.r
@@ -1,22 +1,21 @@
 bracket.shaq = function(x, i, j)
 {
-  ret = x
   if (missing(i))
   {
-    DATA(ret) = Data(x)[, j, drop=FALSE]
+    Data = Data(x)[, j, drop=FALSE]
     if (length(j) == 0 || (length(j) == 1 && j == 0))
-      ret@ncols = 0
+      ncols = 0
     else if (is.logical(j))
-      ret@ncols = length(which(j))
+      ncols = length(which(j))
     else if (j[1] > 0)
-      ret@ncols = max(0, length(unique(j)))
+      ncols = max(0, length(unique(j)))
     else # negative
-      ret@ncols = max(0, ret@ncols - length(unique(j)))
+      ncols = max(0, ncol(x) - length(unique(j)))
   }
   else
     comm.stop("not yet implemented for i non-missing")
   
-  ret
+  shaq(Data, nrow(x), ncols)
 }
 
 
