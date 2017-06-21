b1fcbaa935b26d000657de63de64d46be2e95cdc
Drew Schmidt <wrathematics@gmail.com>
2017-06-21 17:16:12 -0400

    minor print re-org

--- R/print.r
+++ R/print.r
@@ -1,20 +1,3 @@
-print_shaq = function(x, ...)
-{
-  if (comm.rank() == 0)
-  {
-    cat(paste0("# A shaq: ", nrow(x), "x", ncol(x), " on ", comm.size(), " MPI ranks\n"))
-    
-    toprow = min(10, NROW(Data(x)))
-    topcol = min(6, NCOL(Data(x)))
-    submat = Data(x)[1:toprow, 1:topcol]
-    print(submat)
-    
-    cat("# ...\n\n")
-  }
-}
-
-
-
 #' print
 #' 
 #' Print method for a shaq.
@@ -30,8 +13,25 @@ NULL
 
 #' @rdname print
 #' @export
-setMethod("print", signature(x="shaq"), print_shaq)
+setMethod("print", signature(x="shaq"), function(x, ...) print_shaq(x))
 
 #' @rdname print
 #' @export
 setMethod("show", signature(object="shaq"), function(object) print_shaq(object))
+
+
+
+print_shaq = function(x)
+{
+  if (comm.rank() == 0)
+  {
+    cat(paste0("# A shaq: ", nrow(x), "x", ncol(x), " on ", comm.size(), " MPI ranks\n"))
+    
+    toprow = min(10, NROW(Data(x)))
+    topcol = min(6, NCOL(Data(x)))
+    submat = Data(x)[1:toprow, 1:topcol]
+    print(submat)
+    
+    cat("# ...\n\n")
+  }
+}
