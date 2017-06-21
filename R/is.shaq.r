732fb037d5434ee6a7019f3f9c72d8a94b9776ff
wrathematics <wrathematics@gmail.com>
2017-06-21 15:21:11 -0400

    add is.shaq docs

--- R/is.shaq.r
+++ R/is.shaq.r
@@ -1,3 +1,13 @@
+#' is.shaq
+#' 
+#' Test if an object is a shaq.
+#' 
+#' @param x
+#' An R object.
+#' 
+#' @return
+#' A logical value, indicating whether or not the input is a shaq.
+#' 
 #' @export
 is.shaq = function(x)
 {
