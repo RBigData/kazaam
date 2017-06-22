0caa171d6eb0603f18fbcdbac84afdbf5069f855
wrathematics <wrathematics@gmail.com>
2017-06-22 12:27:04 -0400

    first pass pkg description

--- R/pbdSHAQ-package.r
+++ R/pbdSHAQ-package.r
@@ -1,6 +1,13 @@
 #' Tall Matrices
 #' 
-#' TODO
+#' Many data science problems reduce to operations on very tall,
+#' skinny matrices.  However, sometimes these matrices can be so tall that they
+#' are difficult to work with, or do not even fit into main memory.  One
+#' strategy to deal with such objects is to distribute their rows across
+#' several processors.  To this end, we offer a container for tall, skinny,
+#' distributed matrices ('shaqs') in the form of an 'S4' class.  We also
+#' provide many useful numerical methods for operating on these distributed
+#' objects.
 #' 
 #' This package requires an MPI library (OpenMPI, MPICH2, or LAM/MPI).
 #' 
