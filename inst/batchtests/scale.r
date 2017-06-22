9c6a981e666b45affd3b775ea58d18c10ab31e6e
wrathematics <wrathematics@gmail.com>
2017-06-22 10:12:19 -0400

    expand scale tests

--- inst/batchtests/scale.r
+++ inst/batchtests/scale.r
@@ -2,16 +2,23 @@ suppressPackageStartupMessages(library(pbdSHAQ))
 
 if (comm.rank() == 0){
   x = matrix(1:30, 10)
-  s_tf = scale(x, TRUE, FALSE)
+  c_tf = scale(x, TRUE, FALSE)
+  s_tf = scale(x, FALSE, TRUE)
+  cs_tf = scale(x, TRUE, TRUE)
 } else {
   x = NULL
 }
 
 
 dx = expand(x)
-s_tf_test = collapse(scale(dx, TRUE, FALSE))
 
+c_tf_test = collapse(scale(dx, TRUE, FALSE))
+s_tf_test = collapse(scale(dx, FALSE, TRUE))
+cs_tf_test = collapse(scale(dx, TRUE, TRUE))
+
+comm.print(all.equal(c_tf, c_tf_test))
 comm.print(all.equal(s_tf, s_tf_test))
+comm.print(all.equal(cs_tf, cs_tf_test))
 
 
 finalize()
