ecc92a75aace932408f93e27a6d931b0137c3a49
wrathematics <wrathematics@gmail.com>
2017-07-18 15:15:45 -0400

    update crossprod test

--- inst/batchtests/crossprod.r
+++ inst/batchtests/crossprod.r
@@ -1,17 +1,24 @@
 suppressPackageStartupMessages(library(kazaam))
+comm.set.seed(1234)
 
 if (comm.rank() == 0){
   x = matrix(rnorm(30), 10)
-  cp = crossprod(x)
+  y = matrix(rnorm(40), 10)
+  cp_x = crossprod(x)
+  cp_xy = crossprod(x, y)
 } else {
   x = NULL
+  y = NULL
 }
 
-
 dx = expand(x)
+dy = expand(y)
 
-cp_test = crossprod(dx)
+### Tests
+test = crossprod(dx)
+comm.print(all.equal(cp_x, test))
 
-comm.print(all.equal(cp, cp_test))
+test = crossprod(dx, dy)
+comm.print(all.equal(cp_xy, test))
 
 finalize()
