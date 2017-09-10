97824f71d541a9118ac074f86270c0f577e33e89
Drew Schmidt <wrathematics@gmail.com>
2017-09-10 19:51:07 -0400

    fix native routine registration

--- src/kazaam_native.c
+++ src/kazaam_native.c
@@ -5,12 +5,12 @@
 #include <R_ext/Rdynload.h>
 #include <stdlib.h>
 
-extern SEXP R_mpicrossprod(SEXP x);
+extern SEXP R_mpicrossprod(SEXP x, SEXP alpha_);
 extern SEXP R_trinv(SEXP x_, SEXP triang);
 extern SEXP R_km_assign(SEXP x_, SEXP centers_);
 
 static const R_CallMethodDef CallEntries[] = {
-  {"R_mpicrossprod", (DL_FUNC) &R_mpicrossprod, 1},
+  {"R_mpicrossprod", (DL_FUNC) &R_mpicrossprod, 2},
   {"R_trinv", (DL_FUNC) &R_trinv, 2},
   {"R_km_assign", (DL_FUNC) &R_km_assign, 2},
   {NULL, NULL, 0}
